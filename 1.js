47f37279f43fd7d1508a5ceaed11b654f90847d4
Maciek <mkachniarz1@gmail.com>
2018-11-05 20:36:58 +0100

    1 js

---
+++ 1.js
@@ -0,0 +1,22 @@
+var factorial = function fact(number){
+    
+    console.log(Math.floor(number));
+
+    if(number <= 1) return 1;
+ 
+    return Math.floor(number) * fact(Math.floor(number) - 1);
+}
+
+console.log(factorial(6));
+
+
+function facto(number){
+    var n = Math.floor(number);
+
+    var result = 1;
+    for(n; n > 0; n--){
+        result *= n;
+    }
+    return result;
+}
+console.log(facto(6));
