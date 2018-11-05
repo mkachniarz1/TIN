386bf8a991c4da56558d50522befcd3a5847c41c
Maciek <mkachniarz1@gmail.com>
2018-11-05 21:38:58 +0100

    palindrom

---
+++ 3.js
@@ -0,0 +1,13 @@
+function IsPalindrom(sentence) {
+    var sen = sentence.replace(/ /g, '').toLowerCase();
+
+    for (var i = 0; i < sen.length / 2; i++) {
+        if (sen[i] != sen[sen.length - 1 - i]) {
+            console.log("This is not a palindrom");            
+            return;
+        }
+    }
+    console.log("This is a palindrom");
+}
+
+IsPalindrom("Kobyła ma mały bok");
