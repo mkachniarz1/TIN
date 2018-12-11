f971a09901610ab90b051837b46b7e96c7c416ea
Maciej Kachniarz <maciej.kachniarz@predica.pl>
2018-12-11 20:15:00 +0100

    test

--- cw9/1.js
+++ cw9/1.js
@@ -6,7 +6,7 @@ app.get('/hello', (req, res) => {
     res.sendFile(path.join(__dirname + '/form.html'));
     res.send('Hello World')
 });
-
+s
 app.get('/form', (req, res) => {
     res.sendFile(path.join(__dirname + '/form.html'));
 })
