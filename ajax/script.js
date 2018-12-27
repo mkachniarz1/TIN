463618ab310c3765d73537ae7e76ef9a55f1ce44
Maciej Kachniarz <maciej.kachniarz@predica.pl>
2018-12-27 16:36:29 +0100

    delete unnecessary files

--- ajax/script.js
+++
@@ -1,6 +0,0 @@
-let xhr = new XMLHttpRequest()
-
-console.log(xhr);
-
-document.getElementById("pp").innerHTML = "absdakjsdbnkjasd";
-document.getElementById("button").addEventListener('click') = "absdakjsdbnkjasd";
